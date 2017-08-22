3f9715e1498261a93494c667c730196c66d001b2
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-08-22 16:38:50 -0700

    Tried implementing Numbeo API to get cost of living but API doesn't work.

--- apps/HelloWorld/static/HelloWorld/javascript/main.js
+++ apps/HelloWorld/static/HelloWorld/javascript/main.js
@@ -1,1 +1,22 @@
-console.log("hello world!");
+// console.log("hello world!");
+
+$(document).ready(function() {
+    $("#numbeo").click(function() {
+        console.log("I'm clicked!");
+
+        // $.ajax({
+        //     url: 'numbeo',
+        //     success: function(serverResponse) {
+        //         console.log("success. serverResponse:", serverResponse);
+        //         $('#placeholder').html(JSON.stringify(serverResponse))
+        //     }
+        // })
+
+        var url = "http://service.dice.com/api/rest/jobsearch/v1/simple.json";
+        $.get(url, function(res) {
+            console.log(res);
+            var html_string = "<p>" + res.resultItemList.jobTitle + "</p>";
+            $('#placeholder2').html(html_string);
+        }, "json");
+    });
+});
