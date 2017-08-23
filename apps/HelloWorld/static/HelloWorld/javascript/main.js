34feb65236c1e04b2c2ba87afdcabba39fa8d601
Mike Dacanay <migd64@gmail.com>
2017-08-23 12:00:00 -0700

    added target='_black' to each link

--- apps/HelloWorld/static/HelloWorld/javascript/main.js
+++ apps/HelloWorld/static/HelloWorld/javascript/main.js
@@ -1,34 +1,33 @@
 $(document).ready(function(){
     var count = 1;
-    $.get("http://service.dice.com/api/rest/jobsearch/v1/simple.json?page="+count+"", function(res) {
 
+    $.get("http://service.dice.com/api/rest/jobsearch/v1/simple.json?page="+count+"", function(res) {
         for(var i = 0; i < res.resultItemList.length; i++){
         	$('#sidebar').append(
         	"<a href='" + res.resultItemList[i].detailUrl + 
-        	 "'><h4>" + res.resultItemList[i].jobTitle + "</h4></a>"+
+        	 "' target='_blank'><h4>" + res.resultItemList[i].jobTitle + "</h4></a>"+
         	 "<h5>" + res.resultItemList[i].company + "</h5>"+
         	 "<p class = 'job_location'><i>" +"&nbsp"+res.resultItemList[i].location + "</i></p>" +
         	 "<p class = 'job_date'>" + res.resultItemList[i].date + "</p>"
         	)
     		}
-    	}, "json");
+    	    }, "json");
 
     $('#sidebar').on('scroll', function() {
         if(Math.round($(this).scrollTop() + $(this).innerHeight(), 10) >= Math.round($(this)[0].scrollHeight, 10)) {
             count++;
             $.get("http://service.dice.com/api/rest/jobsearch/v1/simple.json?page="+count+"", function(res) {
-
-        for(var i = 0; i < res.resultItemList.length; i++){
-            $('#sidebar').append(
-            "<a href='" + res.resultItemList[i].detailUrl + 
-             "'><h4>" + res.resultItemList[i].jobTitle + "</h4></a>"+
-             "<h5>" + res.resultItemList[i].company + "</h5>"+
-             "<p class = 'job_location'><i>" +"&nbsp"+res.resultItemList[i].location + "</i></p>" +
-             "<p class = 'job_date'>" + res.resultItemList[i].date + "</p>"
-            )
+            for(var i = 0; i < res.resultItemList.length; i++){
+                $('#sidebar').append(
+                "<a href='" + res.resultItemList[i].detailUrl + 
+                 "'><h4>" + res.resultItemList[i].jobTitle + "</h4></a>"+
+                 "<h5>" + res.resultItemList[i].company + "</h5>"+
+                 "<p class = 'job_location'><i>" +"&nbsp"+res.resultItemList[i].location + "</i></p>" +
+                 "<p class = 'job_date'>" + res.resultItemList[i].date + "</p>"
+                )
+                }
+                }, "json");
             }
-        }, "json");
-        }
-    })    
+        })
 })
 
